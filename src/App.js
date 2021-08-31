e49641cfd13173b5c5446380509908b88fa75fb6
Josh Greco <greco.joshua@gmail.com>
2021-08-31 11:12:50 -0400

    website background and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return(
+    <div>
+      <NavBar />
+      <h1 className='App-header'>FUCK YEA!</h1>
+      
     </div>
+    
   );
 }
 
 export default App;
+
+//flexbox
+//grid on css tricks
---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// function navBar(){
+//     return (
+//         <h1 className='testing'>this is where the bar should be</h1>
+//     );
+// }
+
+//navBar = (<DATA>) =>
+export function NavBar() {
+    return(
+        <div>
+            <h1>Testing navBar</h1>
+        </div>
+        
+    );
+}
+
+// export default navBar;
